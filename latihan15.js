8bacd7ef33216a2f0a958b93aef57d236e6ad51f
Restu Ardhi Ansyah S <48643710+RestuArdhi@users.noreply.github.com>
2019-04-09 12:28:21 +0700

    Add files via upload

--- latihan15.js
+++ latihan15.js
@@ -1,44 +1,32 @@
-// Diberikan sebuah function groupAnimals(arr) yang menerima satu parameter berupa array yang berisi string. Function akan me-return array multidimensi dimana array tersebut berisikan kategori/kelompok array yang dikumpulkan sesuai dengan abjad depannya. Abjad harus berurut dari a-z (jika ada).Untuk kasus ini, anggap saja semua teks lowercase.
+// Diberikan sebuah function groupAnimals(arr) yang menerima satu parameter berupa array yang berisi string. Function akan me-return array multidimensi dimana array tersebut berisikan kategori/kelompok array yang dikumpulkan sesuai dengan abjad depannya. Abjad harus berurut dari a-z (jika ada). Untuk kasus ini, anggap saja semua text lowercase.
 
-// Contoh jika arr inputan adalah ["ayam", "kucing", "bebek", "bangau", "zebra"] maka output: [ [ "ayam" ], [ "bebek", "bangau" ], [ "kucing ], [ "zebra" ] ]
+// Contoh jika animals adalah ["ayam", "kucing", "bebek", "bangau", "zebra"]
+
+// maka akan menampilkan output: [ [ "ayam" ], [ "bebek", "bangau" ], [ "kucing ], [ "zebra" ] ]
 
 // Urutan hewan dalam array setiap pengelompokan huruf tidak penting.
-
-//Code
 function groupAnimals(animals) {
-    var namaAkhir = [];
-  
-    for ( var i = 0; i < animals.length; i++) {
-      var animalA = [];
-      var animalC = [];
-      var animalK = [];
-      var animalU = [];
-      
-      for ( var j = 0; j < animals.length; j++) {
-        if( animals[j][0] === 'u') {
-          animalU.push(animals[j]);
-        }else if(animals[j][0] === 'a') {
-          animalA.push(animals[j]);
-        } else if( animals[j][0] === 'c' ) {
-          animalC.push(animals[j]);
-        } else if( animals[j][0] === 'k' && animals[j][1] ==='u') {
-          animalK.push(animals[j]);
-        } 
-      }
+  var tampung = [ [] ];
+  var a = 0;
+  animals.sort()
+  tampung[0].push(animals[0])
+
+  for(var i = 1 ; i < animals.length ; i++){
+    if(animals[i][0] === tampung[a][0][0]){ 
+      tampung[a].push(animals[i])
+    }else{
+      a++;
+      tampung[a] = [animals[i]]
     }
-    namaAkhir.push(animalA);
-    namaAkhir.push(animalC);
-    namaAkhir.push(animalK);
-    namaAkhir.push(animalU);
-    return namaAkhir;
   }
-  // TEST CASES
-  console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil']));
-  // [ ['ayam', 'anoa'], ['cacing'], ['kuda'] ]
-   console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil', 'unta', 'cicak' ]));
-  // [ ['ayam', 'anoa'], ['cacing', 'cicak'], ['kuda'], ['unta'] ]
-  
-  
-  
-  
-  
+  return tampung
+}
+
+// TEST CASES
+console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil']));
+// [ ['ayam', 'anoa'], ['cacing'], ['kuda', 'kancil'] ]
+
+
+////// BUAT TES AJA
+
+console.log(groupAnimals(['kelinci', 'lintah', 'zebra', 'unta', 'badak', 'bebek']));
